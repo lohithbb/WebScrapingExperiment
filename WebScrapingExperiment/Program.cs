cc440db0393b319e155629521b484eb413bd4059
lohithbb <lohithbb@hotmail.com>
2021-01-04 04:07:20 +0000

    settin up the repo; commiting what has been done

---
+++ WebScrapingExperiment/Program.cs
@@ -0,0 +1,90 @@
+﻿using AngleSharp.Html.Dom;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebScrapingExperiment
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region Initial Trials
+
+            //// URL - of the novel ToC
+            //var url = "https://novelfull.com/release-that-witch.html";
+
+            //// create the webscraper object
+            var webScraper = new WebScraper();
+
+            //// get the page as an IDocument
+            //var document = webScraper.GetWebpageAsync(url).Result;
+
+            //var y = document.All
+            //    // get section with the list of chapters
+            //    .Where(e => e.LocalName == "ul" && e.HasAttribute("id") && e.GetAttribute("id") == "list-chapter");
+            ////.Where(e => e.LocalName == "ul" && e.HasAttribute("class") && e.GetAttribute("class") == "row");
+            ////.Where(e => e.LocalName == "a" && e.HasAttribute("href"));
+            ////.Select((e,f) => (e.GetAttribute("href"),(e.GetAttribute("title"))));
+            ////.Where(e => e.LocalName == "a")
+            ////.Select(e => e.LocalName);
+
+            //var z = document
+            //    .QuerySelector("#list-chapter > .row")
+            //    .QuerySelectorAll("a")
+            //    .OfType<IHtmlAnchorElement>()
+            //    //.Select(x => x.GetAttribute("href"))
+            //    .ToList()
+            //    .Select(x => x.Href)
+            ////    .QuerySelectorAll("a");
+            //    ;
+            //// this worked ...
+
+            #endregion Initial Trials
+
+            // so lets try to get a list of URLs for each of the chapter names
+            // once it gets written to file, the call to get chapters can be disabled.
+            // Note that the file will persist in the bin folder
+            #region Get Chapter Urls
+
+            //{
+            //    var listOfChapterUrls = new List<string>();
+
+            //    var urlTableOfContents = "https://novelfull.com/release-that-witch.html";
+            //    listOfChapterUrls = webScraper.GetChapterUrls(urlTableOfContents).Result;
+
+            //    // store in file
+            //    System.IO.File.WriteAllLines("chapterUrls.txt", listOfChapterUrls);
+            //}
+
+            #endregion Get Chapter Urls
+
+
+            #region Experiment - Get content in one chapter
+
+            var inputWorkingWith = "https://novelfull.com/release-that-witch/chapter-1-from-today-onwards-i-am-a-royal-prince.html";
+
+            //var webScraper = new WebScraper();
+            var chapterContent = webScraper.ExtractChapterContent(inputWorkingWith);
+
+            // sanitise
+            // remove leading/trailing whitespace/newlines
+            chapterContent = chapterContent.Trim(System.Environment.NewLine.ToCharArray());
+
+            // markdown treats 2 newlines as a new line ...
+            chapterContent = chapterContent.Replace(System.Environment.NewLine, System.Environment.NewLine + System.Environment.NewLine);
+
+            // write file to disk
+            System.IO.File.WriteAllText("experimentchapter.md", chapterContent);
+
+            #endregion Experiment - Get content in one chapter
+
+            //var urls = System.IO.File.ReadAllLines("chapterUrls.txt");
+
+            // get each chapter and save it
+            //foreach (var chapterUrl in chapterUrls)
+            //{
+            //    var chapterContent =
+            //}
+        }
+    }
+}
---
+++ WebScrapingExperiment/WebScraper.cs
@@ -0,0 +1,116 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScrapingExperiment
+{
+    internal class WebScraper
+    {
+        /// <summary>
+        ///     A HTTP Get request of the URL is performed.
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns>
+        ///     AngleSharp.Dom.IDocument
+        /// </returns>
+        public async Task<IDocument> GetWebpageAsync(string Url)
+        {
+            // Load default configuration
+            var config = Configuration.Default.WithDefaultLoader();
+
+            // Create a new browsing context
+            var context = BrowsingContext.New(config);
+
+            // This is where the HTTP request happens, returns <IDocument> that // we can query later
+            var document = await context.OpenAsync(Url);
+
+            // Check the HTML content that has been returned
+            //Console.WriteLine(document.DocumentElement.OuterHtml);
+
+            // Return the IDocument
+            return document;
+        }
+
+        /// <summary>
+        ///     Returns a list of the URLs for each chapter in the Table of Contents
+        /// </summary>
+        /// <param name="urlTableOfContents"></param>
+        /// <returns>
+        ///     A list of string containing th URLs of each chapter
+        /// </returns>
+        public Task<List<string>> GetChapterUrls(string urlTableOfContents)
+        {
+            // store the URLs; will be returned
+            var listOfChapterUrls = new List<string>();
+
+            var response = GetWebpageAsync(urlTableOfContents).Result;
+
+            do
+            {
+                response = GetWebpageAsync(urlTableOfContents).Result;
+
+                var chapterUrls = response
+                    .QuerySelector("#list-chapter > .row")
+                    .QuerySelectorAll("a")
+                    .OfType<IHtmlAnchorElement>()
+                    .Select(x => x.Href)
+                    .ToList()
+                    ;
+
+                listOfChapterUrls.AddRange(chapterUrls); // as I know it only allows 50 per page
+
+                // move to the next page (as there are 50 chapters per page)
+                // this will error if there is no next page
+                try
+                {
+                    var nextUrl = response
+                                .QuerySelector("li.next")
+                                .QuerySelectorAll("a")
+                                .OfType<IHtmlAnchorElement>()
+                                .Select(x => x.Href)
+                                .First()
+                                ;
+
+                    urlTableOfContents = nextUrl;
+                }
+                catch (System.Exception)
+                {
+                    break;
+                }
+            } while (response.QuerySelector("#disabled") == null);
+
+            return Task.FromResult(listOfChapterUrls);
+        }
+
+        public string ExtractChapterTitle(string urlChapter)
+        {
+            var response = GetWebpageAsync(urlChapter).Result;
+
+            var chapterTitle = response
+                .QuerySelector(".chapter-text")
+                .TextContent
+                ;
+
+            return chapterTitle;
+        }
+
+        public string ExtractChapterContent(string urlChapter)
+        {
+            var response = GetWebpageAsync(urlChapter).Result;
+
+            var chapterContentAsList = response
+                //.QuerySelector("#chapter-content")
+                .QuerySelectorAll("#chapter-content > p")
+                .Select(x => x.TextContent)
+                .ToList()
+                ;
+
+            var chapterContent = string.Join(System.Environment.NewLine, chapterContentAsList);
+
+            return chapterContent;
+        }
+    }
+}
